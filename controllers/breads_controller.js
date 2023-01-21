90651ea0853222ad6d7dd74a0babd38528302f64
Cody Jarrett <cjarrett@newrelic.com>
2023-01-21 16:24:02 -0600

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -6,7 +6,7 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-    res.render('index', {breads: Bread})
+    res.render('index', {breads: Bread, title: 'Index Page'})
 //   res.send(Bread)
 })
 
