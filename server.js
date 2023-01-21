e4bfc38f1358c2c9ed10c542b088517fafe31725
Cody Jarrett <cjarrett@newrelic.com>
2023-01-21 15:11:35 -0600

    sending index and show data

---
+++ server.js
@@ -0,0 +1,23 @@
+// DEPENDENCIES
+const express = require('express')
+
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Welcome to an Awesome App about Breads!')
+})
+
+
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+})
