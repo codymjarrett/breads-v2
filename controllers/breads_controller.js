f6879191cb045ed480676b170cf08dd8512b3955
Cody Jarrett <cjarrett@newrelic.com>
2023-01-25 22:41:17 -0600

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -37,6 +37,15 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
+
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
@@ -61,6 +70,20 @@ breads.delete('/:indexArray', (req, res) => {
   Bread.splice(req.params.indexArray, 1)
   res.status(303).redirect('/breads')
 })
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+
+
+
 
 
 
--- views/show.jsx
+++ views/show.jsx
@@ -13,6 +13,7 @@ function Show ({bread, index}) {
             <form action={`/breads/${index}?_method=DELETE`} method="POST">
               <input type='submit' value="DELETE"/>
             </form>
+            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
             <p>
                 and it
                 {
